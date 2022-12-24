ba2e579c8304c9166c469e403a7235ec01611fa5
SheriffSoco <japenes@gmail.com>
2022-12-23 21:06:08 -0800

    Create basic node server (no express)

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
-const userName = "jacob";
-console.log(userName);
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log("INCOMING REQUEST");
+  console.log(req.method, req.url);
+
+  if (req.method === "POST") {
+    let body = "";
+    req.on("end", () => {
+      console.log(body);
+      const userName = body.split('=')[1];
+      res.end("<h1>" + userName + "</h1>");
+    });
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.end(
+      '<form method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>'
+    );
+  }
+});
+
+server.listen(5001);
