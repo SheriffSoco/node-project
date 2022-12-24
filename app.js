fa3ed2046d46b05020f397c85ca93b4e8eaf0aeb
SheriffSoco <japenes@gmail.com>
2022-12-24 14:13:52 -0800

    Create simple express application

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
-const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const server = http.createServer((req, res) => {
-  console.log("INCOMING REQUEST");
-  console.log(req.method, req.url);
+const app = express();
 
-  if (req.method === "POST") {
-    let body = "";
-    req.on("end", () => {
-      console.log(body);
-      const userName = body.split('=')[1];
-      res.end("<h1>" + userName + "</h1>");
-    });
-    req.on("data", (chunk) => {
-      body += chunk;
-    });
-  } else {
-    res.setHeader("Content-Type", "text/html");
-    res.end(
-      '<form method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>'
-    );
-  }
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/user', (req, res, next) => {
+    res.send('<h1>User: ' + req.body.username + '</h1>');
 });
 
-server.listen(5001);
+app.get('/', (req, res, next) => {
+  res.send(
+    '<form action="/user" method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>'
+  );
+});
+
+app.listen(5001);
